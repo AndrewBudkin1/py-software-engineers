c72ec70f633bfae490d6a86305fac68ab5e19db1
Andrew Budkin <budkin123765@gmail.com>
2026-08-17 15:35:42 +0300

    Solution_updated_2

--- app/main.py
+++ app/main.py
@@ -43,12 +43,12 @@ class AndroidDeveloper(SoftwareEngineer):
 class FullStackDeveloper(BackendDeveloper, FrontendDeveloper):
     def __init__(self, name: str) -> None:
         super().__init__(name)
-        self.skills = ["Python",
+        self.skills.extend(["Python",
                        "SQL",
                        "Django",
                        "JavaScript",
                        "HTML",
-                       "CSS"]
+                       "CSS"])
 
     def create_web_application(self) -> None:
         print(f"{self.name} started creating a web application...")
